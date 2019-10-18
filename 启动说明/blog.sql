e700c4008291cd1ed0b4b0abe516238529527a97
wxw <wangxiaowen2345@163.com>
2019-10-18 09:21:06 +0800

    Update blog.sql

--- 启动说明/blog.sql
+++ 启动说明/blog.sql
@@ -1,18 +1,4 @@
-/*
-SQLyog Ultimate v12.08 (32 bit)
-MySQL - 5.7.26 : Database - zhoubao
-*********************************************************************
-*/
 
-
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 CREATE DATABASE /*!32312 IF NOT EXISTS*/`zhoubao` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE `zhoubao`;
